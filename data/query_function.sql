9f43bb8bad1e6de7c82a84e1d3fd5b912d0350e3
Jakob Miksch <info@jakobmiksch.eu>
2024-10-20 14:53:20 +0200

    WIP

--- data/query_function.sql
+++ data/query_function.sql
@@ -1,9 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS postgisftw;
 
-DROP FUNCTION IF EXISTS postgisftw.osm_website_clone;
+DROP FUNCTION IF EXISTS postgisftw.osm_website_objects_around;
 
 CREATE
-OR REPLACE FUNCTION postgisftw.osm_website_clone (
+OR REPLACE FUNCTION postgisftw.osm_website_objects_around (
   latitude float,
   longitude float,
   radius int,
@@ -37,6 +37,48 @@ OR REPLACE FUNCTION postgisftw.osm_website_clone (
   WHERE ST_DWithin(geog, ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::geography, radius)
   AND not ST_Intersects(geog, ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::geography)
   AND ST_Covers(ST_MakeEnvelope(min_lon, min_lat, max_lon, max_lat, 4326)::geography, geog)
+$$ LANGUAGE sql STABLE PARALLEL SAFE;
+
+DROP FUNCTION IF EXISTS postgisftw.osm_website_objects_enclosing;
+
+CREATE
+OR REPLACE FUNCTION postgisftw.osm_website_objects_enclosing (
+  latitude float,
+  longitude float,
+  min_lat float,
+  min_lon float,
+  max_lat float,
+  max_lon float
+) RETURNS TABLE (
+  osm_type char,
+  osm_id bigint,
+  tags jsonb,
+  geom geometry
+) AS $$
+    SELECT
+    osm_type,
+    osm_id,
+    tags,
+    -- geog::geometry
+    CASE
+        WHEN NOT ST_Covers (
+          ST_MakeEnvelope (min_lon, min_lat, max_lon, max_lat, 4326)::geography,
+          geog
+        ) THEN NULL::geometry
+        ELSE geog::geometry
+    END AS geog
+    FROM (
+      SELECT gw.osm_type, gw.osm_id, r.tags, gw.geog  AS geog
+      FROM geom_ways AS gw
+      JOIN raw_ways AS r ON gw.osm_id = r.id
+
+      UNION
+
+      SELECT gr.osm_type, gr.osm_id, r.tags, gr.geog AS geog
+      FROM geom_rels AS gr
+      JOIN raw_rels AS r ON gr.osm_id = r.id
+  ) AS osm_objects
+  WHERE ST_Covers(geog, ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::geography)
 $$ LANGUAGE sql STABLE PARALLEL SAFE;
 
 DROP FUNCTION IF EXISTS postgisftw.osm_feature_info_geog;
