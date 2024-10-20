becb2fe6358a0f37a9e4ce2de3102fd9e5815744
Jakob Miksch <info@jakobmiksch.eu>
2024-10-20 12:24:06 +0200

    refactor fun

--- data/query_function.sql
+++ data/query_function.sql
@@ -1,15 +1,44 @@
 CREATE SCHEMA IF NOT EXISTS postgisftw;
 
+DROP FUNCTION IF EXISTS postgisftw.osm_website_clone;
+
+CREATE
+OR REPLACE FUNCTION postgisftw.osm_website_clone (latitude float, longitude float, distance int) RETURNS TABLE (
+  osm_type char,
+  osm_id bigint,
+  tags jsonb,
+  geom geometry
+) AS $$
+
+    SELECT osm_type, osm_id, tags, geog::geometry FROM (
+      SELECT gn.osm_type, gn.osm_id, r.tags, gn.geog AS geog
+      FROM geom_nodes AS gn
+      JOIN raw_nodes AS r ON gn.osm_id = r.id
+
+      UNION
+
+      SELECT gw.osm_type, gw.osm_id, r.tags, gw.geog  AS geog
+      FROM geom_ways AS gw
+      JOIN raw_ways AS r ON gw.osm_id = r.id
+
+      UNION
+
+      SELECT gr.osm_type, gr.osm_id, r.tags, gr.geog AS geog
+      FROM geom_rels AS gr
+      JOIN raw_rels AS r ON gr.osm_id = r.id
+  ) AS osm_objects
+  WHERE ST_DWithin(geog, ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::geography, distance)
+$$ LANGUAGE sql STABLE PARALLEL SAFE;
 
 DROP FUNCTION IF EXISTS postgisftw.osm_feature_info_geog;
 
-CREATE OR REPLACE FUNCTION postgisftw.osm_feature_info_geog(
-    latitude float,
-    longitude float,
-    distance int
-)
-RETURNS TABLE(osm_type char, osm_id bigint, tags jsonb, geom geometry) AS
-$$
+CREATE
+OR REPLACE FUNCTION postgisftw.osm_feature_info_geog (latitude float, longitude float, distance int) RETURNS TABLE (
+  osm_type char,
+  osm_id bigint,
+  tags jsonb,
+  geom geometry
+) AS $$
   SELECT gn.osm_type, gn.osm_id, r.tags, gn.geog::geometry
   FROM geom_nodes AS gn
   JOIN raw_nodes AS r ON gn.osm_id = r.id
@@ -28,18 +57,17 @@ $$
   FROM geom_rels AS gr
   JOIN raw_rels AS r ON gr.osm_id = r.id
   WHERE ST_DWithin(gr.geog, ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::geography, distance)
-$$
-LANGUAGE sql STABLE PARALLEL SAFE;
+$$ LANGUAGE sql STABLE PARALLEL SAFE;
 
 DROP FUNCTION IF EXISTS postgisftw.osm_feature_info_webmercator_unprecise;
 
-CREATE OR REPLACE FUNCTION postgisftw.osm_feature_info_webmercator_unprecise(
-    latitude float,
-    longitude float,
-    distance int
-)
-RETURNS TABLE(osm_type char, osm_id bigint, tags jsonb, geom geometry) AS
-$$
+CREATE
+OR REPLACE FUNCTION postgisftw.osm_feature_info_webmercator_unprecise (latitude float, longitude float, distance int) RETURNS TABLE (
+  osm_type char,
+  osm_id bigint,
+  tags jsonb,
+  geom geometry
+) AS $$
   SELECT gn.osm_type, gn.osm_id, r.tags, gn.geom_3857 ::geometry
   FROM geom_nodes AS gn
   JOIN raw_nodes AS r ON gn.osm_id = r.id
@@ -58,5 +86,4 @@ $$
   FROM geom_rels AS gr
   JOIN raw_ways AS r ON gr.osm_id = r.id
   WHERE ST_DWithin(gr.geom_3857, ST_Transform(ST_SetSRID(ST_MakePoint(longitude, latitude), 4326), 3857), distance)
-$$
-LANGUAGE sql STABLE PARALLEL SAFE;
+$$ LANGUAGE sql STABLE PARALLEL SAFE;
