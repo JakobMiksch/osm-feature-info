a6d1be3e6654a2237bc3f7ca11157ca560d8c9cc
Jakob Miksch <info@jakobmiksch.eu>
2024-11-02 14:56:36 +0100

    Refactor

--- import-theme/osm-feature-info/topics/geography_tables.lua
+++ import-theme/osm-feature-info/topics/geography_tables.lua
@@ -2,43 +2,6 @@ local themepark, theme, cfg = ...
 
 themepark:set_option('srid', 4326)
 
-local function has_area_tags(tags)
-    if tags.area == 'yes' then
-        return true
-    end
-    if tags.area == 'no' then
-        return false
-    end
-
-    return tags.aeroway
-        or tags.amenity
-        or tags.building
-        or tags.harbour
-        or tags.historic
-        or tags.landuse
-        or tags.leisure
-        or tags.man_made
-        or tags.military
-        or tags.natural
-        or tags.office
-        or tags.place
-        or tags.power
-        or tags.public_transport
-        or tags.shop
-        or tags.sport
-        or tags.tourism
-        or tags.water
-        or tags.waterway
-        or tags.wetland
-        or tags['abandoned:aeroway']
-        or tags['abandoned:amenity']
-        or tags['abandoned:building']
-        or tags['abandoned:landuse']
-        or tags['abandoned:power']
-        or tags['area:highway']
-        or tags['building:part']
-end
-
 themepark:add_table{
     name = 'geom_nodes',
     geog = 'point',
@@ -91,7 +54,7 @@ themepark:add_table{
 themepark:add_proc('way', function(object, data)
 
     attributes = {}
-    if object.is_closed and has_area_tags(object.tags) then
+    if object.is_closed and theme.has_area_tags(object.tags) then
         attributes = {
             geog = object:as_polygon()
         }
