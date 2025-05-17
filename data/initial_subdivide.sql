9f440b072d3247743d7b6aed2be793f4e28966e8
Jakob Miksch <info@jakobmiksch.eu>
2025-05-17 18:17:06 +0200

    Enable replication for subdivided polygons

---
+++ data/initial_subdivide.sql
@@ -0,0 +1,27 @@
+-- set constant 'max_vertices'
+INSERT INTO osm2pgsql_properties (property, value)
+VALUES ('max_vertices', '256')
+ON CONFLICT (property)
+DO UPDATE SET value = EXCLUDED.value;
+
+CREATE OR REPLACE FUNCTION max_vertices()
+RETURNS INTEGER AS $$
+BEGIN
+    RETURN (SELECT value FROM osm2pgsql_properties where property='max_vertices');
+END;
+$$ LANGUAGE plpgsql;
+
+-- compute subdivided polygons
+TRUNCATE polygons_subdivided; -- ensure table is empty
+-- start subdivide
+INSERT INTO
+    polygons_subdivided (osm_id, osm_type, geom)
+SELECT
+    osm_id,
+    osm_type,
+    ST_SubDivide (geog::geometry, max_vertices()) AS geom
+FROM
+    geometries
+WHERE
+   ST_NPoints(geog::geometry) > max_vertices()
+    AND ST_GeometryType(geog::geometry) IN ('ST_Polygon', 'ST_MultiPolygon');
