4d16e361f509d9626ad17eaffeaa43545ae4354f
Jakob Miksch <info@jakobmiksch.eu>
2025-05-17 16:18:47 +0200

    data type

--- postgres/docker-entrypoint-initdb.d/01_create_postgis.sql
+++ postgres/docker-entrypoint-initdb.d/01_create_postgis.sql
@@ -7,7 +7,7 @@ CREATE TABLE
     polygons_subdivided (
         osm_id int NOT NULL,
         osm_type char NOT NULL,
-        geom geometry NOT NULL
+        geom Geometry(POLYGON, 4326) NOT NULL
     );
 
 CREATE INDEX idx_polygons_subdivided_geom ON polygons_subdivided USING gist (geom);
